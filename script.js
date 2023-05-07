fe5c28c499a09e46ea8f77dc8802e5b303153365
IvannaCheban <ivanna_cheban@ukr.net>
2023-05-07 10:55:34 +0300

    set score element to 0,remove dice

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-'use strict';
+"use strict";
+//Selecting elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1"); //working flow is the same but its considered faster
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+let dice = document.querySelector(".dice").classList.remove("dice");

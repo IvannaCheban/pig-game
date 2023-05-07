41664fd4c809666c9758bfe821958135e261d752
IvannaCheban <ivanna_cheban@ukr.net>
2023-05-07 11:14:01 +0300

    add buttons to a slelected elements

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 //Selecting elements
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
-const diceEl = document.querySelector(".dice"); //working flow is the same but its considered faster
+//working flow is the same but its considered faster
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 //Starting conditions
 score0El.textContent = 0;

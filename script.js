f95bd737e000facb30d99873170385df6de98be8
IvannaCheban <ivanna_cheban@ukr.net>
2023-05-07 11:05:03 +0300

    add starting conditions to the score and dice el

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 "use strict";
 //Selecting elements
 const score0El = document.querySelector("#score--0");
-const score1El = document.getElementById("score--1"); //working flow is the same but its considered faster
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice"); //working flow is the same but its considered faster
 
+//Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
-let diceDisplay = document.querySelector(".dice").classList.add("hidden");
+diceEl.classList.add("hidden");
